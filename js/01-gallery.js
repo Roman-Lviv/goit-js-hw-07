943d5d61ab5044aa2e326aab31eb3b3ba2137322
Roman-Lviv <kituraroman@gmail.com>
2023-09-30 17:10:16 +0300

    git add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,5 @@
 import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
-
-import { galleryItems } from "./gallery-items.js";
-
 const galleryContainer = document.querySelector(".gallery");
 
 function createGalleryItem(item) {
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -24,9 +24,6 @@ function createGalleryItem(item) {
 const galleryMarkup = galleryItems.map(createGalleryItem);
 galleryContainer.append(...galleryMarkup);
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
 const lightbox = new SimpleLightbox(".gallery a", {
   captionsData: "alt",
   captionDelay: 250,
